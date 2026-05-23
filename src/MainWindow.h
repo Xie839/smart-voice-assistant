7cfca92478e475de49c18d69d693864accff5ffe
Xie839 <2119565503@qq.com>
2026-05-23 13:02:59 +0800

    feat: add Qt main window layout

---
+++ src/MainWindow.h
@@ -0,0 +1,85 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "TaskConfig.h"
+
+class QLabel;
+class QTextEdit;
+class QComboBox;
+class QPushButton;
+class QFrame;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+
+    TaskConfig currentTaskConfig() const;
+
+public slots:
+    void appendRawText(const QString &text);
+    void setOptimizedText(const QString &text);
+    void setRunningStatus(const QString &status);
+    void setLastAsrTime(qint64 ms);
+    void setAiConfigured(bool configured, const QString &provider = "");
+
+signals:
+    void startVoiceInputRequested(TaskConfig config);
+    void stopVoiceInputRequested();
+
+    void fileTranscribeRequested(const QString &filePath, TaskConfig config);
+
+    void offlineOptimizeRequested(const QString &rawText);
+    void aiOptimizeRequested(const QString &rawText);
+
+    void copyRequested(const QString &text);
+    void exportRequested(const QString &rawText, const QString &optimizedText);
+    void saveRecordRequested(const QString &rawText, const QString &optimizedText);
+    void clearRequested();
+
+    void openSettingsRequested();
+    void testAiConnectionRequested();
+
+private:
+    void setupUi();
+    void setupConnections();
+    void updateStatusBar();
+    QString chooseLocalFile();
+
+private:
+    QLabel *pageTitleLabel = nullptr;
+    QLabel *pageDescLabel = nullptr;
+    QLabel *runStatusBadge = nullptr;
+    QLabel *statusBarLabel = nullptr;
+    QLabel *aiStatusLabel = nullptr;
+
+    QComboBox *modelComboBox = nullptr;
+    QComboBox *textModeComboBox = nullptr;
+    QComboBox *wordLibComboBox = nullptr;
+
+    QTextEdit *rawTextEdit = nullptr;
+    QTextEdit *optimizedTextEdit = nullptr;
+
+    QPushButton *startButton = nullptr;
+    QPushButton *stopButton = nullptr;
+    QPushButton *fileButton = nullptr;
+    QPushButton *copyButton = nullptr;
+    QPushButton *exportButton = nullptr;
+    QPushButton *saveButton = nullptr;
+    QPushButton *clearButton = nullptr;
+    QPushButton *aiOptimizeButton = nullptr;
+    QPushButton *settingsButton = nullptr;
+    QPushButton *testAiButton = nullptr;
+
+    QString currentPage = "realtime";
+    QString currentStatus = "等待输入";
+    qint64 lastAsrTimeMs = -1;
+
+    bool aiConfigured = false;
+    QString aiProvider = "";
+};
+
+#endif
